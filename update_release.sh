47c46f69d54ed2b9530dbb2093782cba8592f08c
Carlos Merchán Carmona <ccarmona.87@outlook.es>
2025-01-03 18:24:33 +0100

    Fix code heredoc

--- update_release.sh
+++ update_release.sh
@@ -20,22 +20,22 @@ To run the program, you need to provide two arguments:
 
 ### Example Execution
 
-```bash
-    ./pnp_card /path/to/directory cards.pdf
-```
+\`\`\`bash
+./pnp_card /path/to/directory cards.pdf
+\`\`\`
 
 ## Directory Structure
 
-```
+\`\`\`
 /path/to/directory/
 ├── 1_front.png
 ├── 1_back.jpg
 ├── 2_front.jpeg
 ├── global_back.png
-```
+\`\`\`
 
 ### Specific Scenarios
-1.	If 1_front.png exists but 1_back.jpg is missing, the program will use global_back.png as the back image for that card.
-2.	If a back image (name_back) exists without a corresponding front image (name_front), the program will throw an error indicating the issue.
+1. If 1_front.png exists but 1_back.jpg is missing, the program will use global_back.png as the back image for that card.
+2. If a back image (name_back) exists without a corresponding front image (name_front), the program will throw an error indicating the issue.
 
 EOF
